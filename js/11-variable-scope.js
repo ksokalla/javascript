df02cfeaa70a639aa7f06797086d58407861cd22
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-02-04 15:05:57 +0100

    funkcja blad

--- js/11-variable-scope.js
+++ js/11-variable-scope.js
@@ -1,2 +1,31 @@
 'use strict';
 
+/*
+jak nie robić w JaVascript - zmienne powinny mieć inne nazwy niż parametry - zawsze używajmy zmiennych lokalnych w funkcji
+*/
+
+
+	
+var iloscOsoba1 = 3500;
+var iloscOsoba2 = 5000;
+var iloscOsoba3 = 7500;
+var iloscOsoba4 = 2500;
+
+var sumaKasy;
+
+sumaKasy = iloscOsoba1 + iloscOsoba2 + iloscOsoba3;
+
+function policzSumeKasy (iloscOsoba1, iloscOsoba2, iloscOsoba3, wiek) {
+	
+	var sumaKasy;
+	
+	sumaKasy = iloscOsoba1 + iloscOsoba2 + iloscOsoba3 + iloscOsoba4;
+	
+	var osoba = [sumaKasy, wiek]
+	
+	return osoba;
+}
+
+
+
+console.log (policzSumeKasy( iloscOsoba3, iloscOsoba2, sumaKasy, 25));
