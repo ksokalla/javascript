3c39c355b8b7643e9bc2965f86727ebbcbc0929b
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-02-04 13:02:15 +0100

    funkcje init

--- js/9-operators.js
+++ js/9-operators.js
@@ -1,2 +1,45 @@
 'use strict';
 
+/*
+
+modulo wyrzuca resztę z dzielenia
+
+*/
+
+var liczba1 = 13;
+var liczba2 = 5;
+
+var wynik;
+
+wynik = liczba1 % liczba2;
+console.log(wynik);
+
+/* operatory przypisania*/
+
+liczba1 -= 3;
+
+console.log(liczba1);
+
+/* 
+
+
+operatory porównania
+
+
+*/
+
+(2>5) ? console.log("tak") : console.log("nie");
+
+/* powyżej i poniże to to samo - operator warunkowy (ternary)*/
+
+
+if (2>5) {
+	console.log("tak");
+} else {
+	console.log("nie");
+}
+
+
+/* operatory logivzne*/
+
+
