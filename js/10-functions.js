7300d6441daeee8b0fde54bedd99d5a231610fae
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-02-04 14:30:00 +0100

    funckje samodzielnie

--- js/10-functions.js
+++ js/10-functions.js
@@ -1,2 +1,52 @@
 'use strict';
+//
+//var liczba1 = 8;
+//var liczba2 = 10;
+//var wynik;
+//
+//
+//function dodajLiczby(skladowa1, skladowa2) {
+//	wynik = liczba1 + liczba2;
+//}
+//
+//dodajLiczby();
+//
+//console.log(wynik);
 
+var a= 3;
+var b= 4;
+var c= 5;
+
+
+function multiply(skladowa1, skladowa2, skladowa3) {
+	var wynikMnozenia;
+	
+	wynikMnozenia = skladowa1*skladowa2*skladowa3;
+	
+	return wynikMnozenia;
+}
+
+
+
+var skladowaWynik = multiply(a, b, c);
+
+var nowa = -5;
+
+var nowa1 = 0;
+
+function warunek(liczba) {
+	
+	if (liczba >= 0) {
+	
+		console.log("wieksze")
+	
+	} else {
+	
+		document.write("mniejsze")
+	}
+	
+}
+
+warunek(skladowaWynik);
+warunek(nowa);
+warunek(nowa1);
