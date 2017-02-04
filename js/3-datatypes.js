fe9824095b71ea6a76fd48ee96de44d073a0a864
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-02-04 10:35:10 +0100

    array as variable

--- js/3-datatypes.js
+++ js/3-datatypes.js
@@ -1,1 +1,62 @@
-'use strict'
+'use strict';
+var wyplata = 5000;
+var premia = 2000;
+var calkowitaWyplata;
+
+calkowitaWyplata = wyplata + premia;
+
+console.log(calkowitaWyplata);
+
+
+/*
+
+Typ - łańcuch znaków - string
+
+*/
+
+var wyplata = "5000";
+var premia = "2000";
+var calkowitaWyplata;
+
+calkowitaWyplata = wyplata + premia;
+
+console.log(calkowitaWyplata);
+
+/*
+
+Typ - typ logiczny - boolean
+
+*/
+
+var czyJestSmog;
+czyJestSmog = false;
+
+if (czyJestSmog) {
+		console.log("jest SMOG");
+} else {
+		console.log("Nie ma SMOGa");
+	}
+
+/* 
+
+Typ specjalny - undefined / null
+*/
+
+
+var niezdefiniowanaZmienna;
+var nullowaZmienna = null;
+
+console.log(niezdefiniowanaZmienna);
+console.log(nullowaZmienna);
+
+
+/*
+
+operacje na stringach
+
+*/
+
+var imieStudenta = "Michał";
+console.log(imieStudenta.indexOf('c'));
+
+
