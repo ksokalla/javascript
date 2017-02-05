2dd8b6b598a7e80227788f64ee10af8dd19998be
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-02-05 10:49:05 +0100

    json funkcje i odwoływanie się

--- js/14-json.js
+++ js/14-json.js
@@ -1,2 +1,21 @@
 'use strict';
 
+var jsonAuta = {
+	"auta": [
+		{marka: "Renault", moc: "120KM", pojemnosc: "1600cm3"},
+		{marka: "BMW", moc: "160KM", pojemnosc: "2000cm3"},
+		{marka: "Fiat 126p", moc: "70KM", pojemnosc: "900cm3"}
+	]
+}
+
+console.log(jsonAuta);
+
+jsonAuta.auta.forEach(function(auto, i){
+
+	console.log("Auto: " + auto.marka + " " + auto.moc + " " + auto.pojemnosc + (i+1));
+});
+
+jsonAuta.auta.forEach(function(samochody/* ta zmienna jest tymczasowa tylko na potrzeby tej funkcji*/, i) {
+	console.log("Marka " + samochody.marka);
+	console.log("Moc " + samochody.moc);
+})
