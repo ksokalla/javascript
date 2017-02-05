6d589584482f3a155ad03c81269a56427a425f0d
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-02-05 11:12:46 +0100

    koniec

--- js/14-json.js
+++ js/14-json.js
@@ -19,3 +19,20 @@ jsonAuta.auta.forEach(function(samochody/* ta zmienna jest tymczasowa tylko na p
 	console.log("Marka " + samochody.marka);
 	console.log("Moc " + samochody.moc);
 })
+
+var jsonAuta2 = {
+	"auta1": [
+		{marka: "Renault", moc: "120KM", pojemnosc: "1600cm3"},
+		{marka: "BMW", moc: "160KM", pojemnosc: "2000cm3"},
+		{marka: "Fiat 126p", moc: "70KM", pojemnosc: "900cm3"}
+	],
+	"auta2" : [
+		{marka: "Wołga", moc: "120KM", pojemnosc: "1600cm3"},
+		{marka: "Mercedes", moc: "130KM", pojemnosc: "1600cm3"},
+		{marka: "Audi", moc: "150KM", pojemnosc: "1600cm3"}
+	]
+}
+
+jsonAuta2.auta2.forEach(function(autaauta, i){
+	console.log(autaauta.moc);
+})
