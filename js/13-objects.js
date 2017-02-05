92d1662e1ea34343e32a0dd74ee553f96fa5096a
Krzysztof Sokalla <k.sokalla@gmail.com>
2017-02-05 10:20:07 +0100

    finito

--- js/13-objects.js
+++ js/13-objects.js
@@ -19,7 +19,7 @@ console.log(krzysztof.wzrost);
 console.log(kasia.wzrost);*/
 
 
-
+/*
 function Osoba(imie, nazwisko) {
 	
 	this.imie = imie;
@@ -43,4 +43,55 @@ krzysiek.wyswietlInfo();
 
 var kasia = new Osoba('Katarzyna', 'Pruszkowska');
 
-kasia.wyswietlInfo();
+kasia.wyswietlInfo();*/
+
+function Auto(marka) {
+	this.marka = marka;
+	this.pojemnosc;
+	this.moc;
+	this.wyswietlInfo = function() {
+		console.log("Marka: " + this.marka + "\n"
+				   + "Pojemność: " + this.pojemnosc + "\n"
+				   + "Moc: " + this.moc + "\n");
+	}
+}
+
+var auta = [];
+
+var renault = new Auto('Renault');
+
+renault.pojemnosc = "1600cm3";
+renault.moc = "120KM";
+auta.push(renault);
+
+var bmw = new Auto('BMW');
+
+bmw.pojemnosc = "2000cm3";
+bmw.moc = "150KM";
+auta.push(bmw);
+
+var fiat = new Auto('Fiat 126p')
+
+fiat.pojemnosc = "800cm3";
+fiat.moc = "30KM";
+auta.push(fiat);
+
+var dacia = new Auto('Dacia')
+
+dacia.pojemnosc = "1200cm3";
+dacia.moc = "70KM";
+auta.push(dacia);
+
+
+auta.forEach(function(auto, i){
+	auto.wyswietlInfo();
+	console.log("Auto numer: " + (i+1));
+});
+
+/*renault.wyswietlInfo();
+
+bmw.wyswietlInfo();
+
+fiat.wyswietlInfo();
+
+dacia.wyswietlInfo();*/
